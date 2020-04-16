2163d8efd8a95f7b61ba6e80963782c66bfb7248
Yahav Ben Yaakov <yahavb@mail.tau.ac.il>
2020-04-16 03:39:03 +0300

    Changed docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,11 +22,11 @@ from pathlib import Path
 # -- Project information -----------------------------------------------------
 
 project = 'mindreader'
-copyright = '2020, Yahav'
-author = 'Yahav'
+copyright = '2020, Yahav Ben Yaakov'
+author = 'Yahav Ben Yaakov'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,8 +34,7 @@ release = '1.0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'#, 'sphinx_click.ext'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx_click.ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -51,7 +50,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
