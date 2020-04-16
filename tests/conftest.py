67d99c35fe9c6e3db29adace39ef01403cbf722f
Yahav Ben Yaakov <yahavb@mail.tau.ac.il>
2020-04-16 04:01:58 +0300

    travis fix attempt

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ import pytest
 from pathlib import Path
 
 project_root = Path(__file__).parent.parent.absolute()
-sys.path.append(project_root)
-# os.environ["PYTHONPATH"] = f'{project_root}'
+# sys.path.append(project_root)
+os.environ["PYTHONPATH"] = f'{project_root}'
 
 
 @pytest.fixture
