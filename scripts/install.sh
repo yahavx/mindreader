09643e4c76cfd5b8cc4a8407310fb54b9ad16d6d
yahavx <38952809+yahavx@users.noreply.github.com>
2020-04-26 04:24:35 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,7 @@ function main {
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     pip install -r requirements.txt
+    .env/bin/pip install -r requirements.txt
 }
 
 
